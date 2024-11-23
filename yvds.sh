2f439e04810efe10e11e770c2451097d6b978041
Psharkcat <psharkcatbusiness@gmail.com>
2024-11-23 19:05:42 +0100

    update

--- yvds.sh
+++ yvds.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Step 1: Run the Python module `venu` with the argument `venu`
 python -m venu venu&> /dev/null
 
-# Step 2: Activate the virtual environment
 source venv/bin/activate&> /dev/null
 
-# Step 3: Install the `pytubefix` package inside the virtual environment
 pip install pytubefix&> /dev/null
 
-# Step 4: Run `main.py` with the provided arguments (inside the virtual environment)
 python main.py "$1" "$2"
 
-# Optionally, deactivate the virtual environment
 deactivate&> /dev/null
